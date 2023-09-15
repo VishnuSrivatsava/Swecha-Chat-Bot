2ea07205c6c1e46ecd7f12e2b0aa06dff435a051
Vishnu Srivatsava <49309611+VishnuSrivatsava@users.noreply.github.com>
2023-09-15 22:51:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,24 @@ from langchain import HuggingFaceHub
 import requests
 import os
 from time import sleep
+from PIL import Image
 
 from dotenv import load_dotenv
 load_dotenv()
 
 yourHFtoken = os.getenv("YOUR_HF_TOKEN")
 
-av_us = 'images/man.png'
-av_ass = 'images/robot.png'
+av_us = Image.open('images/man.png')
+av_ass = Image.open('images/robot.png')
+
+def writehistory(text):
+    with open('chathistory.txt', 'a') as f:
+        f.write(text)
+        f.write('\n')
+    f.close()
+
+yourHFtoken = "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"
+repo="HuggingFaceH4/starchat-beta"
 
 st.title("Swecha Chat Bot")
 st.subheader("using Starchat-beta")
@@ -31,7 +41,7 @@ def starchat(model,myprompt, your_template):
     prompt = PromptTemplate(template=template, input_variables=["myprompt"])
     llm_chain = LLMChain(prompt=prompt, llm=llm)
     llm_reply = llm_chain.run(myprompt)
-    reply = llm_reply.partition('')[0]
+    reply = llm_reply.partition('<|end|>')[0]
     return reply
 
 
@@ -40,28 +50,24 @@ if "messages" not in st.session_state:
 
 for message in st.session_state.messages:
     if message["role"] == "user":
-        with st.chat_message(message["role"]):
-            st.image(av_us, use_column_width=True, caption="User Avatar")
+        with st.chat_message(message["role"],avatar=av_us):
             st.markdown(message["content"])
     else:
-        with st.chat_message(message["role"]):
-            st.image(av_ass, use_column_width=True, caption="Assistant Avatar")
+        with st.chat_message(message["role"],avatar=av_ass):
             st.markdown(message["content"])
 
 if myprompt := st.chat_input("What is an AI model?"):
     st.session_state.messages.append({"role": "user", "content": myprompt})
-    with st.chat_message("user"):
-        st.image(av_us, use_column_width=True, caption="User Avatar")
+    with st.chat_message("user", avatar=av_us):
         st.markdown(myprompt)
         usertext = f"user: {myprompt}"
         writehistory(usertext)
     with st.chat_message("assistant"):
-        st.image(av_ass, use_column_width=True, caption="Assistant Avatar")
         message_placeholder = st.empty()
         full_response = ""
         res  =  starchat(
                 st.session_state["hf_model"],
-                myprompt, "\n\n\n{myprompt}\n")
+                myprompt, "<|system|>\n<|end|>\n<|user|>\n{myprompt}<|end|>\n<|assistant|>")
         response = res.split(" ")
         for r in response:
             full_response = full_response + r + " "
@@ -71,4 +77,3 @@ if myprompt := st.chat_input("What is an AI model?"):
         asstext = f"assistant: {full_response}"
         writehistory(asstext)       
         st.session_state.messages.append({"role": "assistant", "content": full_response})
-
